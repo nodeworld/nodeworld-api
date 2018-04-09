5f4243e36b090a714f3ae3533b7f66a0628a7c0f
Xavier Brown <secton67@gmail.com>
2018-04-09 14:58:45 -0500

    wait for database to open before starting server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,28 @@
-import { createConnection } from "typeorm";
+import * as util from "util";
+
+const promiseTimeout = util.promisify(setTimeout);
+
+import { createConnection, getConnection } from "typeorm";
 
 import { app } from "./app";
 
 const PORT = process.env.PORT || 2000;
 
+const blockingConnectionWait = async () => {
+    while(true) {
+        try {
+            await createConnection();
+            return;
+        } catch(e) {
+            console.log("Failed to connect to database. Retrying...");
+            await promiseTimeout(3000, async () => await blockingConnectionWait);
+        }
+    }
+}
+
 (async () => {
-    await createConnection();
+    console.log("Connecting to database...");
+    await blockingConnectionWait();
     console.log("Database connection OK");
     app.listen(PORT, () => {
         console.log(`Nodeworld API is now listening on localhost:${PORT}...`);
