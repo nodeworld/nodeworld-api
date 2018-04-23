65f8804618a0a7eb8a8c4beb7799c3f5b413b89a
Xavier Brown <secton67@gmail.com>
2018-04-22 23:20:04 -0500

    lint utils

--- src/seed.ts
+++ src/seed.ts
@@ -1,26 +1,34 @@
-import { createConnection, getConnection } from "typeorm";
 import { generate } from "generate-password";
+import { createConnection, getConnection } from "typeorm";
 
 import { Node } from "./models/node";
 import { Visitor } from "./models/visitor";
-
 import { blockingConnectionWait } from "./server";
+import { logger } from "./utils/log.utils";
 
 (async () => {
     try {
         await blockingConnectionWait();
         const db = getConnection().manager;
-        if(await db.findOne(Node, { name: "main" }))
+        if (await db.findOne(Node, { name: "main" })) {
             await db.remove(await db.findOne(Node, { name: "main" }));
-        if(await db.findOne(Visitor, { name: process.env.MAIN_MANAGER_NAME || "nodeworld" }))
-            await db.remove(await db.findOne(Visitor, { name: process.env.MAIN_MANAGER_NAME || "nodeworld" }));
-        const main_visitor = new Visitor({ name: process.env.MAIN_MANAGER_NAME || "nodeworld" });
-        await main_visitor.setPassword(process.env.MAIN_MANAGER_PASSWORD || generate());
-        const main_node = new Node({ name: "main", owner: main_visitor, greeting: process.env.MAIN_NODE_GREETING || "Welcome to Nodeworld!"});
-        await db.save([main_visitor, main_node]);
-        console.log("Database seeded.");
-    } catch(e) {
-        console.error(e);
+        }
+        if (await db.findOne(Visitor, { name: process.env.MAIN_MANAGER_NAME || "nodeworld" })) {
+            await db.remove(await db.findOne(Visitor, {
+                name: process.env.MAIN_MANAGER_NAME || "nodeworld",
+            }));
+        }
+        const mainVisitor = new Visitor({ name: process.env.MAIN_MANAGER_NAME || "nodeworld" });
+        await mainVisitor.setPassword(process.env.MAIN_MANAGER_PASSWORD || generate());
+        const mainNode = new Node({
+            name: "main",
+            owner: mainVisitor,
+            greeting: process.env.MAIN_NODE_GREETING || "Welcome to Nodeworld!",
+        });
+        await db.save([mainVisitor, mainNode]);
+        logger.info("Database seeded.");
+    } catch (e) {
+        logger.error(e);
     } finally {
         process.exit();
     }
--- src/utils/jwt.utils.ts
+++ src/utils/jwt.utils.ts
@@ -1,5 +1,5 @@
-import * as jwt from "jsonwebtoken";
 import * as fs from "fs";
+import * as jwt from "jsonwebtoken";
 
 import { promisify } from "util";
 
@@ -9,19 +9,28 @@ const PUBLIC_KEY_PATH = process.env.JWT_PUBLIC_KEY_PATH;
 
 export const read = promisify(fs.readFile);
 
-if(!PRIVATE_KEY_PATH) throw new Error("JWT private key path is undefined.");
-if(!PRIVATE_KEY_PASSPHRASE) throw new Error("JWT private key passphrase is undefined.");
-if(!PUBLIC_KEY_PATH) throw new Error("JWT public key passphrase is undefined.");
+if (!PRIVATE_KEY_PATH) throw new Error("JWT private key path is undefined.");
+if (!PRIVATE_KEY_PASSPHRASE) throw new Error("JWT private key passphrase is undefined.");
+if (!PUBLIC_KEY_PATH) throw new Error("JWT public key passphrase is undefined.");
 
 let PRIVATE_KEY: Buffer;
 let PUBLIC_KEY: Buffer;
 
 export const signToken = async (payload: any): Promise<any> => {
-    if(!PRIVATE_KEY) PRIVATE_KEY = await read(PRIVATE_KEY_PATH);
-    return jwt.sign(payload, { key: PRIVATE_KEY.toString(), passphrase: PRIVATE_KEY_PASSPHRASE }, { algorithm: "RS256" })
+    if (!PRIVATE_KEY) PRIVATE_KEY = await read(PRIVATE_KEY_PATH);
+    return jwt.sign(
+        payload,
+        {
+            key: PRIVATE_KEY.toString(),
+            passphrase: PRIVATE_KEY_PASSPHRASE,
+        },
+        {
+            algorithm: "RS256",
+        },
+    );
 };
 
 export const readToken = async (token: string): Promise<any> => {
-    if(!PUBLIC_KEY) PUBLIC_KEY = await read(PUBLIC_KEY_PATH);
-    return jwt.verify(token, PUBLIC_KEY, { algorithms: ["RS256"] })
+    if (!PUBLIC_KEY) PUBLIC_KEY = await read(PUBLIC_KEY_PATH);
+    return jwt.verify(token, PUBLIC_KEY, { algorithms: ["RS256"] });
 };
